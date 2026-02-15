e3a59eb4f8f45cbbaf95ea77590d8af8a84dc12a
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-14 22:56:09 -0500

    game camera

---
+++ scripts/game_camera/game_camera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+
+var target_position: Vector2 = Vector2.ZERO
+
+
+func _ready() -> void:
+	make_current()
+
+
+func _process(delta: float) -> void:
+	acquire_target()
+	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 10))
+
+
+func acquire_target() -> void:
+	var player_nodes: Array[Node] = get_tree().get_nodes_in_group("player")
+	if player_nodes.size() > 0:
+		var player: Player = player_nodes[0]
+		target_position = player.global_position
