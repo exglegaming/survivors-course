0b00dd7275bf70442427e68d0be375e4eac709c0
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-14 22:07:59 -0500

    player movement

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,1 +1,18 @@
+class_name Player
 extends CharacterBody2D
+
+
+const MAX_SPEED: float = 200.0
+
+
+func _physics_process(_delta: float) -> void:
+	var movement_vector: Vector2 = get_movement_vector()
+	var direction: Vector2 = movement_vector.normalized()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+
+func get_movement_vector() -> Vector2:
+	var x_movement: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement: float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement) 
