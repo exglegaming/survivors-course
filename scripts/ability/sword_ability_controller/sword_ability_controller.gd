5a06790042be426d53b3ca6043359fa44dfbffac
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-15 22:35:31 -0500

    targeting enemies with sword ability

--- scripts/ability/sword_ability_controller/sword_ability_controller.gd
+++ scripts/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,6 +1,9 @@
 class_name SwordAbilityController
 extends Node
 
+
+const MAX_RANGE: float = 150.0
+
 @export_category("References")
 @export var sword_ability: PackedScene
 
@@ -15,6 +18,19 @@ func _on_timer_timeout() -> void:
 	var player: Player = get_tree().get_first_node_in_group("player")
 	if player == null: return
 
+	var enemies: Array[Node] = get_tree().get_nodes_in_group("enemy")
+	enemies = enemies.filter(func(enemy: Node2D): 
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
+	)
+
+	if enemies.size() == 0: return
+
+	enemies.sort_custom(func(a: Node2D, b: Node2D):
+		var a_distance: float = a.global_position.distance_squared_to(player.global_position)
+		var b_distance: float = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+
 	var sword_instance: Node2D = sword_ability.instantiate()
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enemies[0].global_position
