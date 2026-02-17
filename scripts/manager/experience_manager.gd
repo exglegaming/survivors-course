1a06330223df863a660ecaa65fbfe3d94172843d
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-16 21:52:44 -0500

    experience tracking

---
+++ scripts/manager/experience_manager.gd
@@ -0,0 +1,18 @@
+class_name ExperienceManager
+extends Node
+
+
+var current_experience: float = 0
+
+
+func _ready() -> void:
+	GameEvents.experience_vial_collected.connect(_on_experience_vial_collected)
+
+
+func increment_experience(number: float) -> void:
+	current_experience += number
+	print(current_experience)
+
+
+func _on_experience_vial_collected(number: float) -> void:
+	increment_experience(number)
