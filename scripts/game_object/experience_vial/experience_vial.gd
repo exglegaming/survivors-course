3510828efc08c0c4c33d60a9ff9b82b1f44c04ce
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-16 21:32:08 -0500

    experience drops

---
+++ scripts/game_object/experience_vial/experience_vial.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+@onready var area_2d: Area2D = $Area2D
+
+
+func _ready() -> void:
+	area_2d.area_entered.connect(_on_area_entered)
+
+
+func _on_area_entered(_area: Area2D) -> void:
+	queue_free()
