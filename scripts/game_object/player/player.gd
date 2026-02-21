38188d699b4f1aa2da781da277274f431f2378a7
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-20 21:05:26 -0500

    adding player health

--- scripts/game_object/player/player.gd
+++ scripts/game_object/player/player.gd
@@ -5,6 +5,18 @@ extends CharacterBody2D
 const MAX_SPEED: float = 125.0
 const ACCELERATION_SMOOTHIN: float = 25.0
 
+var number_colliding_bodies: int = 0
+
+@onready var collision_area_2d: Area2D = $CollisionArea2D
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var damage_interval_timer: Timer = $DamageIntervalTimer
+
+
+func _ready() -> void:
+	collision_area_2d.body_entered.connect(_on_body_entered)
+	collision_area_2d.body_exited.connect(_on_body_exited)
+	damage_interval_timer.timeout.connect(_on_damage_interval_timer_timeout)
+
 
 func _physics_process(delta: float) -> void:
 	var movement_vector: Vector2 = get_movement_vector()
@@ -20,3 +32,23 @@ func get_movement_vector() -> Vector2:
 	var x_movement: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement: float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement) 
+
+
+func check_deal_damage() -> void:
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped(): return
+	health_component.damage(1.0)
+	damage_interval_timer.start()
+	print(health_component.current_health)
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func _on_body_exited(_body: Node2D) -> void:
+	number_colliding_bodies -= 1
+
+
+func _on_damage_interval_timer_timeout() -> void:
+	check_deal_damage()
