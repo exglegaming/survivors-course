4626ab15a4f0e5494cc87157fd0f6731f13b15d2
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-20 21:57:19 -0500

    created a basic victory screen

--- scripts/manager/arena_time_manager.gd
+++ scripts/manager/arena_time_manager.gd
@@ -2,8 +2,19 @@ class_name ArenaTimeManager
 extends Node
 
 
+@export var victory_screen_scene: PackedScene
+
 @onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	timer.timeout.connect(_on_timer_timeout)
 
 
 func get_time_elapsed() -> float:
 	return timer.wait_time - timer.time_left
+
+
+func _on_timer_timeout() -> void:
+	var victory_screen_instance: VictoryScreen = victory_screen_scene.instantiate()
+	add_child(victory_screen_instance)
---
+++ scripts/ui/victory_screen.gd
@@ -0,0 +1,21 @@
+class_name VictoryScreen
+extends CanvasLayer
+
+
+@onready var restart_button: Button = %RestartButton
+@onready var quit_button: Button = %QuitButton
+
+
+func _ready() -> void:
+	get_tree().paused = true
+	restart_button.pressed.connect(_on_restart_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("uid://dua013ovd67cr")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
