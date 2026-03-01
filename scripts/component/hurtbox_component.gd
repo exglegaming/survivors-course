5a16c3aed490353e1d2d7e9255427b9c4bc713eb
ExgleGaming <EaglesFan91x@outlook.com>
2026-03-01 14:21:24 -0500

    added floating damage text

--- scripts/component/hurtbox_component.gd
+++ scripts/component/hurtbox_component.gd
@@ -4,6 +4,8 @@ extends Area2D
 
 @export var health_component: HealthComponent
 
+var floating_text_scene: PackedScene = preload("uid://cvverctnr4k3x")
+
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
@@ -15,3 +17,9 @@ func _on_area_entered(area: Area2D) -> void:
 
 	var hitbox_component: HitboxComponent = area
 	health_component.damage(hitbox_component.damage)
+
+	var floating_text: FloatingText = floating_text_scene.instantiate()
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(int(hitbox_component.damage)))
