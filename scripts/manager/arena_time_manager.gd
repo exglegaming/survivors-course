09259af6ba245654df2457a6b71f60a7096ee696
ExgleGaming <EaglesFan91x@outlook.com>
2026-03-08 18:49:40 -0400

    added victory and defeat jingles

--- scripts/manager/arena_time_manager.gd
+++ scripts/manager/arena_time_manager.gd
@@ -31,3 +31,4 @@ func get_time_elapsed() -> float:
 func _on_timer_timeout() -> void:
 	var end_screen_instance: EndScreen = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
+	end_screen_instance.play_jingle()
